24559ce00b12aadc857e5ecb2899737fd6800117
Johndiddles <johnadepelumi@gmail.com>
2023-12-13 15:31:53 +0100

    finished header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      karla: ["Karla", "sans-serif"],
+      display: ["DM Serif Display", "sans-serif"],
+    },
+    colors: {
+      primary: "rgb(var(--color-primary))",
+      secondary: "rgb(var(--color-secondary))",
+      neutral: "rgb(var(--color-neutral))",
+      accent: "rgb(var(--color-primary-blend))",
+      "dark-primary": "rgb(var(--color-primary-dark))",
+    },
+  },
+  plugins: [],
+};
